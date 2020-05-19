626a18d67f8b1cd364619d92e789d84e8f23de7a
imahnama <muannama25@gmail.com>
2020-05-19 12:45:42 +0300

    Add solutions for all exercises in more join quiz

---
+++ more_join.sql
@@ -0,0 +1,74 @@
+--List the films where the yr is 1962 [Show id, title]
+SELECT id, title
+ FROM movie
+ WHERE yr=1962
+
+ --When was Citizen Kane released?
+ SELECT yr
+FROM movie
+WHERE title = 'Citizen Kane'
+
+--List all of the Star Trek movies, include the id, title and yr (all of these movies include the words Star Trek in the title). Order results by year.
+SELECT id, title, yr
+FROM movie
+WHERE title LIKE  '%Star Trek%'
+
+--id for actor Glenn Close
+SELECT id, name
+FROM actor JOIN casting ON (id = actorid)
+WHERE name = 'Glenn Close'
+
+--id for Casablanca
+SELECT id
+FROM movie
+WHERE title = 'Casablanca'
+
+--Cast list for Casablanca
+SELECT  name
+FROM actor JOIN casting ON (id = actorid)
+WHERE movieid = '27'
+
+--Alien cast list
+SELECT name
+FROM casting
+  JOIN actor ON (actorid = actor.id)
+  JOIN movie ON (movie.id = movieid)
+WHERE
+  title = 'Alien'
+
+--Harrison Ford movies
+SELECT title
+FROM casting
+  JOIN actor ON (actorid = actor.id)
+  JOIN movie ON (movie.id = movieid)
+WHERE
+name = 'Harrison Ford'
+
+--Harrison Ford as a supporting actor
+SELECT
+  title
+FROM casting
+  JOIN actor ON (actorid = actor.id)
+  JOIN movie ON (movie.id = movieid)
+WHERE
+  actor.name = 'Harrison Ford'
+  AND ord != 1
+
+--Lead actors in 1962 movies
+SELECT
+  title,
+  name
+FROM casting
+  JOIN actor ON (actorid = actor.id)
+  JOIN movie ON (movie.id = movieid)
+WHERE
+  yr = 1962
+  AND casting.ord = 1
+
+--Busy years for Rock Hudson
+SELECT yr,COUNT(title) FROM
+  movie JOIN casting ON movie.id=movieid
+        JOIN actor   ON actorid=actor.id
+WHERE name='Rock Hudson'
+GROUP BY yr
+HAVING COUNT(title) > 2
